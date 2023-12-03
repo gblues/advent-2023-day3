6ec2fb821fd06ca645baa698de9ddcbb023c3287
Nathan Strong <gblues@gmail.com>
2023-12-03 12:52:16 -0800

    solution for part 2

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,33 @@
 
 use regex::Regex;
-// 012345678
-// abcdef123
-// start: 6
-// end: 9
+
 fn main() {
-    let pattern = Regex::new(r"\d+").unwrap();
-    let data: &str = include_str!("input/3.txt").strip_suffix("\n").unwrap();
-
-    let grid:Vec<_> = data
-        .split("\n")
-        .map(|line| line.chars().collect::<Vec<_>>()).collect();
+    let gear_pattern = Regex::new(r"\*").unwrap();
+    let num_pattern = Regex::new(r"\d+").unwrap();
+    let input: &str = include_str!("input/3.txt");
+    let data = if input.ends_with("\n") { input.strip_suffix("\n").unwrap() } else {input};
     let mut total: i32 = 0;
-
     let lines:Vec<_> = data.split("\n").collect();
-    for (row, line) in lines.iter().enumerate() {
-        for m in pattern.find_iter(*line) {
-            let begin_x: usize =  if m.range().start > 0 {m.range().start-1} else { 0 };
-            let begin_y: usize = if row > 0 {row-1} else {0};
-            println!("len: {}, end: {}", (*line).len(), m.range().end);
-            let end_x: usize = if m.range().end == (*line).len() {line.len()-1} else {m.range().end};
-            let end_y: usize = if row+1 == lines.len() {row} else {row+1};
-            println!("found: {} at ({}, {}). searching ({}, {}) to ({}, {})",
-                     m.as_str(), m.range().start, row, begin_x, begin_y, end_x, end_y);
-            let mut is_part_number:bool = false;
 
-            for x in begin_x..=end_x {
-                for y in begin_y..=end_y {
-                    match grid[y][x] {
-                        '.' => {},
-                        '0'..='9' => {},
-                        _ => is_part_number = true
-                    }
-                }
+    for (row, line) in lines.iter().enumerate() {
+        for g in gear_pattern.find_iter(*line) {
+            let min: usize = if g.range().start > 0 {g.range().start -1} else {0};
+            let max: usize = g.range().end;
+            let mut lines_to_search: Vec<&str> = Vec::new();
+
+            if row > 0 { lines_to_search.push(lines[row-1]);}
+            lines_to_search.push(*line);
+            if row < lines.len()-1 { lines_to_search.push(lines[row+1]);}
+            let mut part_numbers: Vec<i32> = Vec::new();
+
+            for search in lines_to_search {
+                num_pattern.find_iter(search).filter(
+                    |n| (n.range().end > min) && (n.range().start <= max)
+                ).for_each(|part| part_numbers.push(part.as_str().parse::<i32>().unwrap())
+                );
             }
-            if is_part_number {
-                total += m.as_str().parse::<i32>().unwrap();
+            if part_numbers.len() == 2 {
+                total += part_numbers.iter().product::<i32>();
             }
         }
     }
